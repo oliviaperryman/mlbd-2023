a610167b0db317472d4e1c8a08f41c9f160b3fb4
Olivia Perryman <oliviap.soccer@gmail.com>
2023-02-22 09:44:05 +0100

    Part a

--- tutorials/week-01/challenge.py
+++ tutorials/week-01/challenge.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import pandas as pd
 
 def person_a():
     """Reads the data in data/school_performance.csv
@@ -8,10 +9,8 @@ def person_a():
     Returns:
     dataframe: containing first 5,000 rows of school_performace.csv
     """
-    # Code goes over here.
-
-    
-    raise NotImplementedError()
+    data = pd.read_csv("data/school_performance.csv")
+    return data.head(5000)
 
 def person_b(df):
     """Keeps only the data from the female students. 
